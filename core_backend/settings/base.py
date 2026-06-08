373439ab37b1489a1582c1ad118c201fb0ed0c45
isaacigohe <isaacigohe120@gmail.com>
2026-06-08 15:21:02 +0300

    Adding AUTH_USER_MODEL

--- core_backend/settings/base.py
+++ core_backend/settings/base.py
@@ -85,7 +85,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL =  'users.User'
 
 # ── Django REST Framework ────────────────────────────────────────────────────
 REST_FRAMEWORK = {
