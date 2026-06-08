0628664cdc0d779625249cd57291b6772b233c0d
isaacigohe <isaacigohe120@gmail.com>
2026-06-08 19:35:59 +0300

    created files for users models

---
+++ users/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    # Your custom fields go here (e.g., bio, profile_picture, etc.)
+    pass
