e96cc1df0e624dc3de85d99f5e35dd8a67aa6c32
isaacigohe <isaacigohe120@gmail.com>
2026-06-19 00:32:21 +0300

    fix: enable whitenoise production static storage

--- core_backend/settings/base.py
+++ core_backend/settings/base.py
@@ -88,6 +88,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
