f9f226910d046655ba075d8ba7583c6e4b949787
isaacigohe <isaacigohe120@gmail.com>
2026-06-19 11:50:11 +0300

    fix create_admin code structure

--- create_admin.py
+++ create_admin.py
@@ -1,20 +1,18 @@
 import os
 import django
 
-# Make sure this matches the exact folder name containing your settings.py file
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GlobalScholar.settings')
+# This tells Django to look inside your 'core_backend' folder for settings.py
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core_backend.settings')
 django.setup()
 
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 email = "isaac@gmail.com"
-password = "YourSecurePassword123!"  # Put the password you want here
+password = "Secure1234"  # Put the password you want here
 
 if not User.objects.filter(email=email).exists():
     User.objects.create_superuser(email=email, password=password)
     print("🚀 SUPERUSER CREATED SUCCESSFULLY VIA SCRIPT!")
 else:
     print("✅ Superuser already exists.")
-    
-    
