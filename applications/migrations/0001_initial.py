34de36cd73bfcbd860dd3e5b727e875a3f3967f4
isaacigohe <isaacigohe120@gmail.com>
2026-06-19 11:04:31 +0300

    add missing university migrations

--- applications/migrations/0001_initial.py
+++ applications/migrations/0001_initial.py
@@ -12,6 +12,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('universities', '__first__'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('users', '0001_initial'),
     ]
 
     operations = [
