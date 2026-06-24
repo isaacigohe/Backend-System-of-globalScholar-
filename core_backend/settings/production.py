e48671abff024ee9ad710dc0ca8ac73d4091cfc0
isaacigohe <isaacigohe120@gmail.com>
2026-06-24 14:37:17 +0300

    Fix database connection timeout for Render free tier

--- core_backend/settings/production.py
+++ core_backend/settings/production.py
@@ -26,7 +26,7 @@ if raw_db_url.startswith('postgres://'):
 DATABASES = {
     'default': dj_database_url.config(
         default=raw_db_url,
-        conn_max_age=600,
+        conn_max_age=0,
     )
 }
 
