1ab1554a7e53f15b5b479482161d1b927edfba09
isaacigohe <isaacigohe120@gmail.com>
2026-06-24 19:32:37 +0300

    add build script for migrations and admin setup

--- build.sh
+++ build.sh
@@ -2,4 +2,18 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py migrate
+
+# Hardcoding the password via a bash pipe
+python -c "
+import os, django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core_backend.settings.production')
+django.setup()
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(email='isaac@globalscholar.com').exists():
+    User.objects.create_superuser('isaac@globalscholar.com', 'Scholar2026Secure', first_name='Isaac', last_name='Admin')
+    print('Superuser created successfully!')
+else:
+    print('Superuser already exists.')
+"
