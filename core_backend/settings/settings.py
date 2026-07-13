3efe37eb84d0779c0894efe715895ed86a86c8d1
isaacigohe <isaacigohe120@gmail.com>
2026-07-13 09:16:50 +0300

    fix: update ACCESS_TOKEN_LIFETIME and REFRESH_TOKEN_LIFETIME in SIMPLE_JWT settings

--- core_backend/settings/settings.py
+++ core_backend/settings/settings.py
@@ -145,8 +145,8 @@ REST_FRAMEWORK = {
 
 # ── SimpleJWT ────────────────────────────────────────────────────────────────
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1), # Changed from 60 minutes
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7), # Changed from 60 minutes
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
